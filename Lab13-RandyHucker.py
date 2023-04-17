ab270165829132b73fc024bd74fe8e8de0e13c33
RandyHucker <rhucker@physna.com>
2023-04-17 12:37:06 -0400

    Update Lab13-RandyHucker.py

---
+++ Lab13-RandyHucker.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+from random import random, seed
+
+# SEEDING THE RANDOM-NUMBER GENERATOR FOR REPRODUCIBILITY
+# seed(43)
+# for roll in range(20):
+#     print(1 + int(random()*6), end=' ')
+
+
+# A COMPUTATIONAL EXPERIMENT
+# sum, sumdev = 0.0, 0.0
+# N = 10000
+# for i in range(N):
+#     next = random.random()
+#     sum += next
+#     dev = (next - 0.5)**2
+#     sumdev += dev
+# mean = sum / N
+# print(f"Mean:{mean}")
+# var = sumdev/(N-1)
+# print(f"Variance: {var}")
+
+
+# MONTE CARLO ESTIMATION OF PI
+# N = 10000
+# circle_hits = 0
+# for i in range(N):
+#     x, y = random(), random()
+#     if x**2 + y**2 <= 1:
+#         circle_hits += 1
+# pi_est = 4 * circle_hits / N
+# print("Final Estimate of Pi=", pi_est)
+
+
+# MONTE CARLO INTEGRATION
+# import numpy as np
+# # limits of integration are a, b
+# a = 0
+# b = np.pi # gets the value of pi
+# N = 10000
+# integral = 0.0
+# def f(x):
+#     return np.sin(x)
+# for i in range (N):
+#     x = a + (b-a)*random()
+#     integral += f(x)
+# ans = (b-a) * integral/N
+# print (f"{ans} is the estimated value.")
+
+
+# BALLS AND PINS SIMULATION
+# import numpy as np
+# balls = np.arange(100, 100000, 100)
+# maxbin = []
+# for N in balls:
+#     bins = np.zeros(N)
+#     for b in range(N):
+#         bins[int(N * random())] += 1
+#     maxbin.append(max(bins))
+# plt.plot(balls, maxbin)
+# plt.show()
+
+print("Done")
